9f0067a7e20faa3bda9f072a0e1e329892247255
Luis Fernando Acosta Soto <luis.fer.acosta99@gmail.com>
2024-02-22 20:06:51 -0600

    modularity
    conenction and closing of session was moved to sepparate functions, main call was removed, and the loop for keyboard interrupt was implemented.

--- openBCI/functions_openBCI.py
+++ openBCI/functions_openBCI.py
@@ -1,37 +1,51 @@
+######### import libraries #########
+####################################
 from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds
+import time
+import numpy as np
 
+######### Manage CYTON #########
+####################################
 def connect_to_cyton():
     # Set up parameters for Cyton board
-    params = BrainFlowInputParams()
-    params.serial_port = 'COM5'  # Update this with your Cyton board's serial port (you can see it in the openBCI GUI)
-
-    # Initialize the board
-    board_id = BoardIds.CYTON_DAISY_BOARD.value
-
-    # Connect to the board
-    board = BoardShim(board_id, params)
-    board.prepare_session()
-    
-    # Start streaming data
-    board.start_stream()
-    
-    return board
-
-# Step 4: Confirm the connection
-def confirm_connection(board):
-    if board.is_prepared():
-        print("Connection to Cyton board established successfully!")
-    else:
-        print("Failed to establish connection to Cyton board.")
+	params = BrainFlowInputParams()
+	params.serial_port = 'COM5'  # Update this with your Cyton board's serial port (you can see it in the openBCI GUI)
+	# Initialize the board instance
+	board_id = BoardIds.CYTON_DAISY_BOARD.value
+	# Connect to the board
+	board = BoardShim(board_id, params)
+	board.prepare_session()
+	return board
 
 # Main function to execute the connection
-def main():
-    board = connect_to_cyton()
-    confirm_connection(board)
-    
-    #
-    print(board.get_board_id())
-    
-    # 
-    board.stop_stream()
-    board.release_session()
+def open_board():
+	board = connect_to_cyton()
+	# Start streaming data
+	board.start_stream()
+	confirm_connection(board)
+	return board
+
+def confirm_connection(board):
+	if board.is_prepared():
+		print("Connection to Cyton board established successfully!")
+	else:
+		print("Failed to establish connection to Cyton board.")
+
+def close_board(board):
+	# stop streaming and disconnect from the board so a new session can be started without having to turn it on and off.
+	board.stop_stream()
+	board.release_session()
+	print("Session ended")
+
+######### Manage DATA #########
+####################################
+# Step 4: Confirm the connection
+
+def signal_acquisition(board):
+	while True:
+		buffer_data = board.get_board_data()
+		height, width = buffer_data.shape
+		print("Height of the array:", height)
+		print("Width of the array:", width)
+		time.sleep(2)
+	return buffer_data
