3fd35f4c148f2d7e721cae9491e19ad669a3a605
Luis Fernando Acosta Soto <luis.fer.acosta99@gmail.com>
2024-04-21 16:27:54 -0600

    Open bci stage2 (#8)
    * requirements .txt
    
    * fixed file saving bug and created functionality for multiple channel plotting

--- openBCI/plot.py
+++ openBCI/plot.py
@@ -6,31 +6,48 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from PyQt5.QtWidgets import QMainWindow, QApplication, QVBoxLayout, QWidget
 from PyQt5 import QtCore
 import board_handler
+import matplotlib
+matplotlib.use('QtAgg')
+
+#from PyQt6 import QtCore, QtWidgets
+from matplotlib.figure import Figure
+
+class MplCanvas(FigureCanvas):
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+
+        # generate axes for each subplot
+        self.axes1 = fig.add_subplot(311)
+        self.axes2 = fig.add_subplot(312)
+        super(MplCanvas, self).__init__(fig)
 
 class PlotUpdater(QMainWindow):
     def __init__(self):
-        super().__init__()
+        super(PlotUpdater, self).__init__()
         self.setWindowTitle("EEG electrodes");
         self.setGeometry(100, 100, 800, 600);
 
-        self.central_widget = QWidget();
-        self.setCentralWidget(self.central_widget);
-
-        self.layout = QVBoxLayout();
-        self.central_widget.setLayout(self.layout);
-
         self.fig, self.ax = plt.subplots();
-        self.canvas = FigureCanvas(self.fig);
-        self.layout.addWidget(self.canvas);
+        ###self.canvas = FigureCanvas(self.fig);
+        self.canvas = MplCanvas(self, width=5, height=4, dpi=100)
+        self.setCentralWidget(self.canvas)
 
         # initiate connection to board and begin acquisition
         self.openbci = board_handler.brainflow_board_object();
         
-        self.x = np.zeros(1);  # assuming 1000 samples initially
-        self.y = np.zeros(1);  # assuming 1000 samples initially
+        self.x = np.zeros(50);  # assuming 1000 samples initially
+        self.y1 = np.zeros(50);  # assuming 1000 samples initially
+        self.y2 = np.zeros(50);  # assuming 1000 samples initially
 
-        #initializes the plot, and intializes the self.line attribute, which is the function line itself 
-        self.line, = self.ax.plot(self.x, self.y);
+        # We need to store a reference to the plotted line
+        # somewhere, so we can apply the new data to it.
+        self._plot_ref1 = None
+        self._plot_ref2 = None
+
+        self.update_plot()
+
+        self.show()
         # Initialize the timer attribute
         self.animation_timer = QtCore.QTimer(self);
 
@@ -50,26 +67,36 @@ class PlotUpdater(QMainWindow):
 
     def update_plot(self):
         # Get new data from the sensor
-        new_data = self.openbci.get_signal();  # Get 100 new samples (adjust as needed)
-        # Append new data to the existing data
-        try:
-            self.y = np.append(self.y,new_data);
-        except TypeError:
-            self.y = np.append(self.y, np.array([new_data]));
-        try:
-            self.x = np.arange(0, len(self.y) * 0.5, 0.5);
-            #self.x  = np.arange(0, self.y.shape[1] * 0.5, 0.5)
-        except IndexError:
-            self.x = np.arange(0, len(self.y) * 0.5, 0.5);
-        # Update the data of the line plot
-        self.line.set_ydata(self.y);
-        self.line.set_xdata(self.x);
+        batch_samples, new_data = self.openbci.get_signal();  # Get 100 new samples (adjust as needed)
 
-        # Adjust plot limits to keep the signal visible
-        self.ax.set_xlim(1, len(self.y) * 0.5)  # adjust x-limit based on the length of data
-    # Adjust y-limit according to the range of data, you may customize this according to your data
-        self.ax.set_ylim(self.y.min()-self.y.min()*0.1, self.y.max()+self.y.min()*0.1)
+        # select the corresponding channel data
+        self.y1 = np.append(self.y1[1:], [new_data[0]]);
+        self.y2 = np.append(self.y2[1:], [new_data[1]]);
 
+        self.x = np.append(self.x[1:], [self.x[-1]+(batch_samples/125)]);
+
+        #print("after updating X",np.shape(self.x), self.x)
+        #print("after updating Y",np.shape(self.y1), self.y1)
+
+        # Plotting signals
+        if self._plot_ref1 is None:
+            plot_refs = self.canvas.axes1.plot(self.x, self.y1, 'r')
+            self._plot_ref1 = plot_refs[0]
+            plot_refs = self.canvas.axes2.plot(self.x, self.y2, 'b')
+            self._plot_ref2 = plot_refs[0]
+            
+        else:
+            self._plot_ref1.set_xdata(self.x)
+            self._plot_ref1.set_ydata(self.y1)
+            self._plot_ref2.set_xdata(self.x)
+            self._plot_ref2.set_ydata(self.y2)
+
+        # Adjust plot limits for subplots
+        self.canvas.axes1.set_xlim(self.x.min() - 1, self.x.max() + 1)
+        self.canvas.axes1.set_ylim(self.y1.min() * 1.15, self.y1.max() * 1.15)
+        self.canvas.axes2.set_xlim(self.x.min() - 1, self.x.max() + 1)
+        self.canvas.axes2.set_ylim(self.y2.min() * 1.15, self.y2.max() * 1.15)
+        
         # Redraw the plot
         self.canvas.draw()
 
