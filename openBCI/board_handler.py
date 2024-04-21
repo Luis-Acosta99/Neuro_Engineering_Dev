8c243467e60450e3bcf998545e4b7bff9140841e
Luis Fernando Acosta Soto <luis.fer.acosta99@gmail.com>
2024-04-21 16:24:18 -0600

    fixed file saving bug and created functionality for multiple channel plotting

--- openBCI/board_handler.py
+++ openBCI/board_handler.py
@@ -11,7 +11,7 @@ class brainflow_board_object():
         params.serial_port = 'COM5'  # Update this with your Cyton board's serial port (you can see it in the openBCI GUI)
         
         # Initialize the board instance
-        btype = "cyton"
+        btype = "other"
         if btype == "cyton":
             board_id = BoardIds.CYTON_DAISY_BOARD.value
         else:
@@ -26,7 +26,7 @@ class brainflow_board_object():
         self.board.start_stream()
         self.confirm_connection()
 
-        self.signal = np.empty((1, 1))
+        self.signal = np.empty((1, 1));
 
     def confirm_connection(self):
         if self.board.is_prepared():
@@ -43,9 +43,24 @@ class brainflow_board_object():
 
     def get_signal(self):
         # cyton daisy returns a [32, 125 p/second] nd array 
-        self.signal = self.board.get_board_data();
-        np.savetxt(self.file, self.signal, delimiter=",")
-        # single electrode mode
-        print(self.signal.shape)
-        return self.signal[6][-1]
-    
+        self.signal = np.transpose(self.board.get_board_data());
+        np.savetxt(self.file, self.signal, delimiter=",") 
+        
+        # print statement for debugging purposes
+        #print(self.session_samples,self.signal.shape)
+
+        return (self.signal.shape[0] ,self.signal[-1][6:8])
+        '''{'accel_channels': [17, 18, 19],
+            'analog_channels': [27, 28, 29],
+            'ecg_channels': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+            'eeg_channels': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+            'eeg_names': 'Fp1,Fp2,C3,C4,P7,P8,O1,O2,F7,F8,F3,F4,T7,T8,P3,P4',
+            'emg_channels': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+            'eog_channels': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+            'marker_channel': 31,
+            'name': 'CytonDaisy',
+            'num_rows': 32,
+            'other_channels': [20, 21, 22, 23, 24, 25, 26],
+            'package_num_channel': 0,
+            'sampling_rate': 125,
+            'timestamp_channel': 30}'''
